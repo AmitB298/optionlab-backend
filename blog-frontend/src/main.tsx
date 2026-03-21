f5653a4e50bf7fb5288e3e1629c95313d116715c
Your Name <exit>
2026-03-22 00:59:23 +0530

    fix: add CSS import to main.tsx

--- blog-frontend/src/main.tsx
+++ blog-frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -7,5 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 )
+
 
 
